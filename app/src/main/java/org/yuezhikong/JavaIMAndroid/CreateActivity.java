31c3dc8d9710283fc7686a0011eb1efee1a9dc7a
QiLechan <qilechan@outlook.com>
2023-11-18 22:20:35 +0800

    添加了添加服务器界面的公钥输入区域和右上角的保存按钮，但尚未完成处理方法

--- app/src/main/java/org/yuezhikong/JavaIMAndroid/CreateActivity.java
+++ app/src/main/java/org/yuezhikong/JavaIMAndroid/CreateActivity.java
@@ -2,6 +2,8 @@ package org.yuezhikong.JavaIMAndroid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -25,8 +27,14 @@ public class CreateActivity extends AppCompatActivity {
                 Intent backIntent = new Intent(this, MainActivity.class);
                 backIntent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                 startActivity(backIntent);
-                break;
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_main, menu);
+        return true;
     }
 }
