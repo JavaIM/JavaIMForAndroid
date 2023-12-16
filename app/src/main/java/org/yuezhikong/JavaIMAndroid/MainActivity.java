0b81607f7d9b3c882c95010b9a8b0485dfe79bbb
QiLechan <qilechan@outlook.com>
2023-12-16 18:43:26 +0800

    修复了第一次打开时闪退的问题

--- app/src/main/java/org/yuezhikong/JavaIMAndroid/MainActivity.java
+++ app/src/main/java/org/yuezhikong/JavaIMAndroid/MainActivity.java
@@ -86,14 +86,14 @@ public class MainActivity extends AppCompatActivity {
         File SavedServerFile = new File(Application.getInstance().getFilesDir().getPath()+"/SavedServers.json");
         SavedServerFileLayout layout = new SavedServerFileLayout();
         Gson gson = new Gson();
+        if (CreateServerList())
+            CheckServerList();
         try {
             layout = gson.fromJson(FileUtils.readTxt(SavedServerFile, StandardCharsets.UTF_8).toString()
                     , SavedServerFileLayout.class);
         } catch (JsonSyntaxException | IOException e) {
-        Toast.makeText(this, "无法解析保存的服务器文件，请检查文件内容", Toast.LENGTH_SHORT).show();
-    }
-        if (CreateServerList())
-            CheckServerList();
+            Toast.makeText(this, "无法解析保存的服务器文件，请检查文件内容", Toast.LENGTH_SHORT).show();
+        }
         if (!(layout.getServerInformation().isEmpty())){
             ShowServerList();
         }
