2b3e3c4d9a3e19e9540146549dba4d324a9e6e68
AlexLiuDev233 <wzylin11@outlook.com>
2024-07-22 10:04:23 +0800

    feat: NonCrashThreadUncaughtExceptionHandler继承类必须调用父类

--- app/src/main/java/org/yuezhikong/JavaIMAndroid/utils/NonCrashThreadUncaughtExceptionHandler.java
+++ app/src/main/java/org/yuezhikong/JavaIMAndroid/utils/NonCrashThreadUncaughtExceptionHandler.java
@@ -3,8 +3,10 @@ package org.yuezhikong.JavaIMAndroid.utils;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.CallSuper;
 
 public class NonCrashThreadUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler{
+    @CallSuper
     @Override
     public void uncaughtException(@NonNull Thread thread, @NonNull Throwable throwable) {
         Log.d("JavaIM Thread",thread.getName()+"出现错误"+throwable.getMessage());
